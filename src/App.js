42b4f2b067f3cce39c248f153877755e485429db
IrsanJam <irsandjamaludin@gmail.com>
2023-09-10 10:38:46 +0700

    update

--- src/App.js
+++ src/App.js
@@ -36,8 +36,8 @@ export const App = () => {
     return filmPopuler.map((movie, i) => {
       return (
         <>
-          <div className="relative box-border h-72 w-60 overflow-y-scroll  rounded-md bg-amber-50 text-green-100 shadow-xl  lg:h-[25rem] lg:w-64" key={i}>
-            <img src={`${gambar}/${movie.poster_path}`} alt="" className="h-40 w-full rounded-t-md lg:h-60 " />
+          <div className="relative box-border h-80 w-72 overflow-y-scroll  rounded-md bg-amber-50 text-green-100 shadow-xl  lg:h-[25rem] lg:w-64" key={i}>
+            <img src={`${gambar}/${movie.poster_path}`} alt="" className="h-[17rem] w-full rounded-t-md lg:h-60 " />
             <h3 className="  bg-green-600 py-3 text-center font-bold ">{movie.title}</h3>
             <hr className="mx-auto w-24 border-green-500" />
             <p className=" overflow-hidden p-3 text-justify text-[9px]  -tracking-tight text-slate-700">{movie.overview}</p>
@@ -70,10 +70,10 @@ export const App = () => {
     <div>
       <div className=" navbar fixed z-10 flex w-full flex-col items-center justify-center  p-10 shadow-lg  ">
         <p className="p-2 text-[13px] font-bold uppercase ">Landing Page Pencarian Film</p>
-        <h1 className="text-center font-bold uppercase text-primary lg:text-4xl">Find... movie Present</h1>
+        <h1 className="text-center text-[1.4em] font-bold uppercase text-primary lg:text-4xl">Find... movie Present</h1>
         <p className="p-2  text-[10px] font-bold text-slate-400">Cari film Favorit anda dengan api terbaik</p>
         <div className="flex items-center justify-center">
-          <input type="text" placeholder="Masukan nama film" className="inputa rounded-xl border-[1px] border-teal-200 px-5 text-center lg:px-32 lg:py-2"></input>
+          <input type="text" placeholder="Masukan nama film" className="inputa rounded-xl border-[1px] border-teal-200 px-2 text-center lg:px-32 lg:py-2"></input>
           <button onClick={eksekusi} className=" ml-3 rounded-xl bg-primary px-5 py-[0.2em] text-sm text-green-50 lg:px-10 lg:py-2 lg:font-medium">
             Cari
           </button>
